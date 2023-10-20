6e2317cabdb3be8fe5af3ffed4ea0b8749a11b15
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-20 11:38:42 -0300

    Padronizando uso de aspas simples

--- endpoints/usuario_get.php
+++ endpoints/usuario_get.php
@@ -8,15 +8,15 @@ function api_usuario_get($request) {
     $user_meta = get_user_meta($user_id);
 
     $response = array(
-      "id" => $user->user_login,
-      "nome" => $user->display_name,
-      "email" => $user->user_email,
-      "cep" => $user_meta['cep'][0],
-      "numero" => $user_meta['numero'][0],
-      "rua" => $user_meta['rua'][0],
-      "bairro" => $user_meta['bairro'][0],
-      "cidade" => $user_meta['cidade'][0],
-      "estado" => $user_meta['estado'][0],
+      'id' => $user->user_login,
+      'nome' => $user->display_name,
+      'email' => $user->user_email,
+      'cep' => $user_meta['cep'][0],
+      'numero' => $user_meta['numero'][0],
+      'rua' => $user_meta['rua'][0],
+      'bairro' => $user_meta['bairro'][0],
+      'cidade' => $user_meta['cidade'][0],
+      'estado' => $user_meta['estado'][0],
     );
   } else {
     $response = new WP_Error('permissao', 'Usuário não possui permissão', array('status' => 401));
--- functions.php
+++ functions.php
@@ -2,15 +2,15 @@
 
 $template_diretorio = get_template_directory();
 
-require_once($template_diretorio . "/custom-post-type/produto.php");
-require_once($template_diretorio . "/custom-post-type/transacao.php");
+require_once($template_diretorio . '/custom-post-type/produto.php');
+require_once($template_diretorio . '/custom-post-type/transacao.php');
 
-require_once($template_diretorio . "/endpoints/usuario_post.php");
-require_once($template_diretorio . "/endpoints/usuario_get.php");
-require_once($template_diretorio . "/endpoints/usuario_put.php");
+require_once($template_diretorio . '/endpoints/usuario_post.php');
+require_once($template_diretorio . '/endpoints/usuario_get.php');
+require_once($template_diretorio . '/endpoints/usuario_put.php');
 
-require_once($template_diretorio . "/endpoints/produto_post.php");
-require_once($template_diretorio . "/endpoints/produto_get.php");
+require_once($template_diretorio . '/endpoints/produto_post.php');
+require_once($template_diretorio . '/endpoints/produto_get.php');
 
 function get_produto_id_by_slug($slug) {
   $query = new WP_Query(array(
